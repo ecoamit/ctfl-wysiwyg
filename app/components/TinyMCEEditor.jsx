a25fd3cf61d7c37d188771afd88923a32e3cddcc
Amit Singh <42237965+ecoamit@users.noreply.github.com>
2024-06-12 15:42:56 +0530

    Update TinyMCEEditor.jsx

--- app/components/TinyMCEEditor.jsx
+++ app/components/TinyMCEEditor.jsx
@@ -1,14 +1,35 @@
-// components/TinyMCEEditor.js
-import React, { useEffect, useState } from "react";
-import { Editor } from "@tinymce/tinymce-react";
-import htmlToRichText from "../utils/htmlToRichText";
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic';
+import htmlToRichText from '../utils/htmlToRichText';
+
+// Dynamically load the TinyMCE editor to prevent SSR issues
+const Editor = dynamic(() => import('@tinymce/tinymce-react').then((mod) => mod.Editor), {
+  ssr: false,
+});
 
 const TinyMCEEditor = ({ sdk }) => {
-  const [value, setValue] = useState(sdk.field.getValue() || "");
+  const [value, setValue] = useState('');
 
   useEffect(() => {
+    const initialValue = sdk.field.getValue();
+    if (initialValue && initialValue.nodeType) {
+      // If initial value is already rich text, convert it to HTML
+      const blocks = initialValue.content.map(block => block.content.map(textNode => textNode.value).join('\n')).join('\n');
+      setValue(blocks);
+    } else {
+      setValue(initialValue || '');
+    }
+
     const detachValueChangeHandler = sdk.field.onValueChanged((newValue) => {
-      setValue(newValue);
+      if (newValue && newValue.nodeType) {
+        // If new value is rich text, convert it to HTML
+        const blocks = newValue.content.map(block => block.content.map(textNode => textNode.value).join('\n')).join('\n');
+        setValue(blocks);
+      } else {
+        setValue(newValue || '');
+      }
     });
 
     sdk.window.startAutoResizer();
@@ -31,9 +52,8 @@ const TinyMCEEditor = ({ sdk }) => {
       init={{
         height: 500,
         menubar: false,
-        plugins: "link image code",
-        toolbar:
-          "undo redo | formatselect | bold italic | alignleft aligncenter alignright | code",
+        plugins: 'link image code',
+        toolbar: 'undo redo | formatselect | bold italic | alignleft aligncenter alignright | code',
       }}
       onEditorChange={handleEditorChange}
     />
